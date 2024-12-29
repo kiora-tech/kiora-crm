a953ccb4456209340c16d5800308c58c602b46d2
Stephane Rathgeber <stephane@kiora.tech>
2024-12-29 22:39:26 +0100

    refacto controller

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -3,92 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Comment;
-use App\Entity\Customer;
-use App\Form\CommentType;
-use App\Repository\CommentRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Routing\Requirement\Requirement;
 
-#[Route('/comment')]
-final class CommentController extends AbstractController
+#[Route('/comment', name: 'app_comment')]
+final class CommentController extends CustomerInfoController
 {
-    #[Route(name: 'app_comment_index', methods: ['GET'])]
-    public function index(CommentRepository $commentRepository): Response
-    {
-        return $this->render('comment/index.html.twig', [
-            'comments' => $commentRepository->findAll(),
-        ]);
-    }
-
-    #[Route('/new/{customer?}', name: 'app_comment_new', methods: ['GET', 'POST'])]
-    public function new(
-        Request $request,
-        EntityManagerInterface $entityManager,
-        ?Customer $customer = null
-    ): Response {
-        $comment = new Comment();
-        $comment->setCustomer($customer);
-
-        $form = $this->createForm(CommentType::class, $comment, ['customer' => $customer]);
-        $form->handleRequest($request);
-
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($comment);
-            $entityManager->flush();
-
-            if ($customer) {
-                return $this->redirectToRoute('app_customer_show', ['id' => $customer->getId()],
-                    Response::HTTP_SEE_OTHER);
-            }
-
-            return $this->redirectToRoute('app_comment_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('comment/new.html.twig', [
-            'comment' => $comment,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_comment_show', methods: ['GET'])]
-    public function show(Comment $comment): Response
-    {
-        return $this->render('comment/show.html.twig', [
-            'comment' => $comment,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_comment_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Comment $comment, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(CommentType::class, $comment);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_comment_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('comment/edit.html.twig', [
-            'comment' => $comment,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_comment_delete', methods: ['POST'])]
-    public function delete(Request $request, Comment $comment, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $comment->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($comment);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_comment_index', [], Response::HTTP_SEE_OTHER);
-    }
+   public function getEntityClass(): string
+   {
+       return Comment::class;
+   }
 }
--- src/Controller/EnergyController.php
+++ src/Controller/EnergyController.php
@@ -2,87 +2,14 @@
 
 namespace App\Controller;
 
-use App\Entity\Customer;
 use App\Entity\Energy;
-use App\Form\EnergyType;
-use App\Repository\EnergyRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/energy')]
-final class EnergyController extends AbstractController
+#[Route('/energy', name: 'app_energy')]
+final class EnergyController extends CustomerInfoController
 {
-    #[Route(name: 'app_energy_index', methods: ['GET'])]
-    public function index(EnergyRepository $energyRepository): Response
-    {
-        return $this->render('energy/index.html.twig', [
-            'energies' => $energyRepository->findAll(),
-        ]);
-    }
-
-    #[Route('/new/{customer?}', name: 'app_energy_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager, ?Customer $customer = null): Response
-    {
-        $energy = new Energy();
-        $energy->setCustomer($customer);
-        $form = $this->createForm(EnergyType::class, $energy, ['customer' => $customer]);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($energy);
-            $entityManager->flush();
-
-            if ($customer) {
-                return $this->redirectToRoute('app_customer_show', ['id' => $customer->getId()],
-                    Response::HTTP_SEE_OTHER);
-            }
-
-            return $this->redirectToRoute('app_energy_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('energy/new.html.twig', [
-            'energy' => $energy,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_energy_show', methods: ['GET'])]
-    public function show(Energy $energy): Response
-    {
-        return $this->render('energy/show.html.twig', [
-            'energy' => $energy,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_energy_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Energy $energy, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(EnergyType::class, $energy);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_energy_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('energy/edit.html.twig', [
-            'energy' => $energy,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_energy_delete', methods: ['POST'])]
-    public function delete(Request $request, Energy $energy, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$energy->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($energy);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_energy_index', [], Response::HTTP_SEE_OTHER);
-    }
+   public function getEntityClass(): string
+   {
+       return Energy::class;
+   }
 }
